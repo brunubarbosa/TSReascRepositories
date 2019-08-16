1fdcb08aff147df4e8e8f27e5b4d85ae35bcc580
Bruno <brunopjsw@gmail.com>
2019-08-16 01:19:25 -0300

    first part

---
+++ src/store/ducks/repositories/index.ts
@@ -0,0 +1,25 @@
+import { RepositoriesState, RepositoriesTypes } from './types';
+import { Reducer } from 'redux';
+
+const INITIAL_STATE: RepositoriesState = {
+  data: [{ id: 1, name: 'rocketseat' }],
+  error: false,
+  loading: false
+};
+
+const reducer: Reducer<RepositoriesState> = (state = INITIAL_STATE, action) => {
+  switch (action.type) {
+    case RepositoriesTypes.LOAD_REQUEST:
+      return { ...state, loading: true };
+
+    case RepositoriesTypes.LOAD_SUCCES:
+      return {...state, loading: false, error: false, data: action.payload.data}
+
+    case RepositoriesTypes.LOAD_FAILURE:
+      return { ...state, loading: false, error: true, data: [] };
+    default:
+      return state;
+  }
+}
+
+export default reducer;
